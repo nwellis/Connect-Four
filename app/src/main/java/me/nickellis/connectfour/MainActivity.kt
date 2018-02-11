d3e40cb6465cc75628a1279bf5e4438790fee8a1
Nick Ellis <nicholas.w.ellis@gmail.com>
2018-02-11 09:53:59 -0600

    Add board UI

--- app/src/main/java/me/nickellis/connectfour/MainActivity.kt
+++ app/src/main/java/me/nickellis/connectfour/MainActivity.kt
@@ -1,11 +1,56 @@
 package me.nickellis.connectfour
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import me.nickellis.connectfour.data.Board
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val board = Board()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    (0 until  board.numOfCols()).forEach {
+      val vColumn = vBoard.addColumn()
+      (0 until board.numOfRows()).forEach {
+        vColumn.addCell()
+      }
+    }
+  }
+
+  fun ViewGroup.addColumn(): ViewGroup {
+    val vColumn = layoutInflater.inflate(
+      R.layout.board_column, this, false
+    ) as LinearLayout
+
+    this.addView(vColumn)
+    return vColumn
+  }
+
+  fun ViewGroup.addCell(): View {
+    val vImageView = layoutInflater.inflate(
+      R.layout.board_cell, this, false
+    ) as ImageView
+
+    vImageView.setBackgroundResource(
+      if (Random().nextInt(1) == 0) R.drawable.cell_black
+      else R.drawable.cell_red
+    )
+
+    val rnd = Random()
+    val color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+    vImageView.setBackgroundColor(color)
+
+    this.addView(vImageView)
+    return vImageView
   }
 }
--- app/src/main/java/me/nickellis/connectfour/data/Board.kt
+++ app/src/main/java/me/nickellis/connectfour/data/Board.kt
@@ -5,8 +5,11 @@ import java.util.*
 class Board : ReadOnlyBoard {
 
   override fun getColumn(column: Int): List<Piece> = columns[column].map { it }
-  override fun numOfCols(): Int = 7
-  override fun numOfRows(): Int = 6
+  override fun numOfCols(): Int = defaultColumns
+  override fun numOfRows(): Int = defaultRows
+
+  private val defaultColumns = 7
+  private val defaultRows = 6
 
   private val columns = Array(numOfCols()) { Stack<Piece>() }
 
@@ -20,8 +23,4 @@ class Board : ReadOnlyBoard {
     columns[column].push(piece)
     whosTurn = if (piece == Piece.Black) Piece.Red else Piece.Black
   }
-
-
-
-
 }
