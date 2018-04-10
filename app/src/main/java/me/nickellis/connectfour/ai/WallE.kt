a5aea87b9db96c0498ea911f7f80aae9e2ff81d7
Nick Ellis <nicholas.w.ellis@gmail.com>
2018-04-09 20:58:25 -0500

    Wall-E blocks winning moves

--- app/src/main/java/me/nickellis/connectfour/ai/WallE.kt
+++ app/src/main/java/me/nickellis/connectfour/ai/WallE.kt
@@ -23,17 +23,29 @@ class WallE(piece: Piece) : Player(piece, false), AI {
   }
 
   private fun rate(pieces: List<List<Piece>>, move: Int, toWin: Int): Int {
-    return pieces.putPiece(move, piece)
+    val againstRating = Piece.values()
+      .filter { it != Piece.Empty && it != piece }
+      .map { Pair(it, pieces.putPiece(move, it).allWinLines(toWin)) }
+      .sumBy {
+        val piece = it.first
+        it.second.sumBy {
+          val consecutive = it.consecutive(piece)
+          if (consecutive >= toWin) Move.PreventWin.weight
+          else Move.Neutral.weight
+        }
+      }
+
+    val forRating = pieces.putPiece(move, piece)
       .allWinLines(4)
       .sumBy {
         it.consecutive()
           .map {
             when(it.first) {
               piece -> {
+                if (it.second >= toWin) return Int.MAX_VALUE
                 when (it.second) {
                   2 -> Move.Two
                   3 -> Move.Three
-                  toWin -> Move.Win
                   else -> Move.Neutral
                 }
               }
@@ -43,12 +55,14 @@ class WallE(piece: Piece) : Player(piece, false), AI {
           }
           .sumBy { it.weight }
       }
+
+    return againstRating + forRating
   }
 }
 
 enum class Move(val weight: Int) {
-  Win(1000),
-  PreventWin(Win.weight - 1),
+  //Win(100000), this is always max
+  PreventWin(100000),
   Three(300),
   Two(100),
   Neutral(0)
